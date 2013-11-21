6b4377d66b0f40f1db321e97f2ed652a0b9e4789
lt <lt@95e5b859-71cd-234a-b975-0ecc2f9501a4>
2013-11-21 14:15:59 +0000

    新增HostStatusChecker工程
    git-svn-id: svn://svn.csdn.net/src/source/HostStatusChecker@911 95e5b859-71cd-234a-b975-0ecc2f9501a4

---
+++ HostStatusChecker_single/HostStatusChecker_single.dsp
@@ -0,0 +1,124 @@
+# Microsoft Developer Studio Project File - Name="HostStatusChecker_single" - Package Owner=<4>
+# Microsoft Developer Studio Generated Build File, Format Version 6.00
+# ** DO NOT EDIT **
+
+# TARGTYPE "Win32 (x86) Application" 0x0101
+
+CFG=HostStatusChecker_single - Win32 Debug
+!MESSAGE This is not a valid makefile. To build this project using NMAKE,
+!MESSAGE use the Export Makefile command and run
+!MESSAGE 
+!MESSAGE NMAKE /f "HostStatusChecker_single.mak".
+!MESSAGE 
+!MESSAGE You can specify a configuration when running NMAKE
+!MESSAGE by defining the macro CFG on the command line. For example:
+!MESSAGE 
+!MESSAGE NMAKE /f "HostStatusChecker_single.mak" CFG="HostStatusChecker_single - Win32 Debug"
+!MESSAGE 
+!MESSAGE Possible choices for configuration are:
+!MESSAGE 
+!MESSAGE "HostStatusChecker_single - Win32 Release" (based on "Win32 (x86) Application")
+!MESSAGE "HostStatusChecker_single - Win32 Debug" (based on "Win32 (x86) Application")
+!MESSAGE 
+
+# Begin Project
+# PROP AllowPerConfigDependencies 0
+# PROP Scc_ProjName ""
+# PROP Scc_LocalPath ""
+CPP=cl.exe
+MTL=midl.exe
+RSC=rc.exe
+
+!IF  "$(CFG)" == "HostStatusChecker_single - Win32 Release"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 0
+# PROP BASE Output_Dir "Release"
+# PROP BASE Intermediate_Dir "Release"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 0
+# PROP Output_Dir "Release"
+# PROP Intermediate_Dir "Release"
+# PROP Ignore_Export_Lib 0
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /YX /FD /c
+# ADD CPP /nologo /MD /W3 /GX /Z7 /O2 /D "NDEBUG" /D "WIN32" /D "_WINDOWS" /D "_UNICODE" /D "UNICODE" /D _WIN32_WINNT=0x500 /D WINVER=0x500 /YX /FD /c
+# ADD BASE MTL /nologo /D "NDEBUG" /mktyplib203 /win32
+# ADD MTL /nologo /D "NDEBUG" /mktyplib203 /win32
+# ADD BASE RSC /l 0x804 /d "NDEBUG"
+# ADD RSC /l 0x804 /d "NDEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:windows /machine:I386
+# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /entry:"wWinMainCRTStartup" /subsystem:windows /pdb:"../Release/HostStatusChecker_single.pdb" /debug /machine:I386 /out:"../Release/HostStatusChecker_single.exe"
+# SUBTRACT LINK32 /pdb:none
+
+!ELSEIF  "$(CFG)" == "HostStatusChecker_single - Win32 Debug"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 1
+# PROP BASE Output_Dir "Debug"
+# PROP BASE Intermediate_Dir "Debug"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 1
+# PROP Output_Dir "Debug"
+# PROP Intermediate_Dir "Debug"
+# PROP Ignore_Export_Lib 0
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /YX /FD /GZ /c
+# ADD CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "_DEBUG" /D "WIN32" /D "_WINDOWS" /D "_UNICODE" /D "UNICODE" /D _WIN32_WINNT=0x500 /D WINVER=0x500 /FR /YX /FD /GZ /c
+# ADD BASE MTL /nologo /D "_DEBUG" /mktyplib203 /win32
+# ADD MTL /nologo /D "_DEBUG" /mktyplib203 /win32
+# ADD BASE RSC /l 0x804 /d "_DEBUG"
+# ADD RSC /l 0x804 /d "_DEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:windows /debug /machine:I386 /pdbtype:sept
+# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /entry:"wWinMainCRTStartup" /subsystem:windows /debug /machine:I386 /out:"../Debug/HostStatusChecker_single.exe" /pdbtype:sept
+
+!ENDIF 
+
+# Begin Target
+
+# Name "HostStatusChecker_single - Win32 Release"
+# Name "HostStatusChecker_single - Win32 Debug"
+# Begin Group "Source Files"
+
+# PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
+# Begin Source File
+
+SOURCE=.\HostStatusChecker_single.rc
+# End Source File
+# Begin Source File
+
+SOURCE=.\hscs.cpp
+# End Source File
+# End Group
+# Begin Group "Header Files"
+
+# PROP Default_Filter "h;hpp;hxx;hm;inl"
+# End Group
+# Begin Group "Resource Files"
+
+# PROP Default_Filter "ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe"
+# Begin Source File
+
+SOURCE=.\res\gray.ico
+# End Source File
+# Begin Source File
+
+SOURCE=.\res\green.ico
+# End Source File
+# Begin Source File
+
+SOURCE=.\res\MainFrame.ico
+# End Source File
+# End Group
+# End Target
+# End Project
---
+++ HostStatusChecker_single/hscs.cpp
@@ -0,0 +1,485 @@
+#define _WIN32_IE 0x500
+
+#include <WinSock2.h>
+#include <MSTcpIP.h>
+#include <Windows.h>
+#include <Shlwapi.h>
+#include <tchar.h>
+#include "resource.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Shlwapi.lib")
+
+#define NOTIFYWNDCLASS TEXT("__slx_HostStatusChecker_single_20131121")
+
+static enum
+{
+    WM_CALLBACK = WM_USER + 112,
+    WM_REPORT,
+};
+
+static enum
+{
+    MI_ABOUT = 11,
+    MI_USEINFO,
+    MI_QUIT,
+};
+
+static char g_szDomainName[64] = "";
+static DWORD g_dwAddress = 0;
+static USHORT g_usPort = 0;
+
+static BOOL InitWinSock(WORD ver)
+{
+    WSADATA wd;
+    int nTryTime = 0;
+
+    while (WSAStartup(ver, &wd) != 0)
+    {
+        Sleep(1000);
+        nTryTime += 1;
+
+        if (nTryTime > 20)
+        {
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
+BOOL IsIpAddress(LPCSTR lpAddress)
+{
+    char szAddress[100];
+    DWORD dwAddress = inet_addr(lpAddress);
+
+    lstrcpynA(szAddress, inet_ntoa(*(in_addr *)&dwAddress), RTL_NUMBER_OF(szAddress));
+
+    return lstrcmpiA(szAddress, lpAddress) == 0;
+}
+
+BOOL IsPortW(LPCWSTR lpPort, USHORT *pusPort)
+{
+    WCHAR szPort[100];
+    int nPort = StrToIntW(lpPort);
+
+    if (nPort < 0 || nPort > 65535)
+    {
+        return FALSE;
+    }
+
+    wnsprintfW(szPort, RTL_NUMBER_OF(szPort), L"%d", nPort);
+
+    if (lstrcmpiW(lpPort, szPort) != 0)
+    {
+        return FALSE;
+    }
+
+    if (pusPort != NULL)
+    {
+        *pusPort = (USHORT)nPort;
+    }
+
+    return TRUE;
+}
+
+int MessageBoxFormat(HWND hWindow, LPCTSTR lpCaption, UINT uType, LPCTSTR lpFormat, ...)
+{
+    TCHAR szText[2000];
+    va_list val;
+
+    va_start(val, lpFormat);
+    wvnsprintf(szText, sizeof(szText) / sizeof(TCHAR), lpFormat, val);
+    va_end(val);
+
+    return MessageBox(hWindow, szText, lpCaption, uType);
+}
+
+void SafeDebugMessage(LPCTSTR lpFormat, ...)
+{
+    TCHAR szText[2000];
+    va_list val;
+
+    va_start(val, lpFormat);
+    wvnsprintf(szText, sizeof(szText) / sizeof(TCHAR), lpFormat, val);
+    va_end(val);
+
+    OutputDebugString(szText);
+}
+
+BOOL SetSockKeepAlive(
+    SOCKET s,
+    ULONG  onoff,
+    ULONG  keepalivetime,
+    ULONG  keepaliveinterval
+    )
+{
+    struct tcp_keepalive tka;
+    DWORD dwBytesReturned = 0;
+    
+    tka.onoff = onoff;
+    tka.keepalivetime = keepalivetime;
+    tka.keepaliveinterval = keepaliveinterval;
+    
+    return 0 == WSAIoctl(
+        s,
+        SIO_KEEPALIVE_VALS,
+        &tka,
+        sizeof(tka),
+        NULL,
+        0,
+        &dwBytesReturned,
+        NULL,
+        NULL
+        );
+}
+
+static LRESULT __stdcall NotifyWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    static NOTIFYICONDATA nid = {sizeof(nid)};
+    static HICON hGreenIco = NULL;
+    static HICON hGrayIco = NULL;
+    static UINT WM_REBUILDTOOLBAR = 0;
+    static BOOL bShowInfo = TRUE;
+
+    if (uMsg == WM_CREATE)
+    {
+        LPCREATESTRUCT lpCs = (LPCREATESTRUCT)lParam;
+
+        hGreenIco = LoadIcon(lpCs->hInstance, MAKEINTRESOURCE(IDI_GREEN));
+        hGrayIco = LoadIcon(lpCs->hInstance, MAKEINTRESOURCE(IDI_GRAY));
+
+        WM_REBUILDTOOLBAR = RegisterWindowMessage(TEXT("TaskbarCreated"));
+
+        //add icon to tray
+        nid.hWnd = hWnd;
+        nid.uID = 1;
+        nid.uCallbackMessage = WM_CALLBACK;
+        nid.hIcon = hGreenIco;
+        nid.uFlags = NIF_TIP | NIF_MESSAGE | NIF_ICON;
+        lstrcpyn(nid.szInfoTitle, TEXT("信息"), sizeof(nid.szTip));
+        nid.uTimeout = 3000;
+        nid.dwInfoFlags = NIIF_INFO;
+
+        Shell_NotifyIcon(NIM_ADD, &nid);
+
+        //
+        PostMessage(hWnd, WM_REPORT, FALSE, FALSE);
+
+        return 0;
+    }
+    else if (uMsg == WM_REPORT)
+    {
+        BOOL bActive = (BOOL)wParam;
+        BOOL bShowInfoThisTime = (BOOL)lParam;
+
+        nid.uFlags = NIF_ICON | NIF_TIP;
+
+        wnsprintf(
+            nid.szTip,
+            RTL_NUMBER_OF(nid.szTip),
+            TEXT("域名：%hs\r\n")
+            TEXT("IP地址：%hs\r\n")
+            TEXT("端口：%u\r\n")
+            TEXT("当前状态：%s\r\n"),
+            g_szDomainName,
+            inet_ntoa(*(in_addr *)&g_dwAddress),
+            g_usPort,
+            bActive ? TEXT("活动") : TEXT("不活动")
+            );
+
+        if (bActive)
+        {
+            nid.hIcon = hGreenIco;
+        }
+        else
+        {
+            nid.hIcon = hGrayIco;
+        }
+
+        if (bShowInfoThisTime && bShowInfo)
+        {
+            wnsprintf(
+                nid.szInfo,
+                RTL_NUMBER_OF(nid.szInfo),
+                TEXT("主机“%hs:%u”已进入%s活动状态。"),
+                lstrlenA(g_szDomainName) > 0 ? g_szDomainName : inet_ntoa(*(in_addr *)&g_dwAddress),
+                g_usPort,
+                bActive ? TEXT("") : TEXT("不")
+                );
+
+            nid.uFlags |= NIF_INFO;
+        }
+
+        Shell_NotifyIcon(NIM_MODIFY, &nid);
+    }
+    else if (uMsg == WM_CLOSE)
+    {
+        Shell_NotifyIcon(NIM_DELETE, &nid);
+        DestroyWindow(hWnd);
+    }
+    else if (uMsg == WM_DESTROY)
+    {
+        PostQuitMessage(0);
+    }
+    else if (uMsg == WM_CALLBACK)
+    {
+        if (lParam == WM_RBUTTONUP)
+        {
+            POINT pt;
+
+            GetCursorPos(&pt);
+
+            SetForegroundWindow(hWnd);
+
+            HMENU hMenu = CreatePopupMenu();
+
+            AppendMenu(hMenu, MF_STRING, MI_ABOUT, TEXT("关于(&A)..."));
+            AppendMenu(hMenu, MF_SEPARATOR, 0, NULL);
+            AppendMenu(hMenu, MF_STRING, MI_USEINFO, TEXT("启用气泡通知(&B)"));
+            AppendMenu(hMenu, MF_STRING, MI_QUIT, TEXT("退出(&Q)"));
+
+            if (bShowInfo)
+            {
+                CheckMenuItem(hMenu, MI_USEINFO, MF_BYCOMMAND | MF_CHECKED);
+            }
+            else
+            {
+                CheckMenuItem(hMenu, MI_USEINFO, MF_BYCOMMAND | MF_UNCHECKED);
+            }
+
+            switch (TrackPopupMenu(hMenu, TPM_RETURNCMD | TPM_NONOTIFY, pt.x, pt.y, 0, hWnd, NULL))
+            {
+            case MI_ABOUT:
+                MessageBox(hWnd, TEXT("HostStatusChecker_single application"), TEXT("信息"), MB_SYSTEMMODAL | MB_ICONINFORMATION);
+                break;
+
+            case MI_USEINFO:
+                bShowInfo = !bShowInfo;
+                break;
+
+            case MI_QUIT:
+                PostMessage(hWnd, WM_CLOSE, 0, 0);
+                break;
+
+            default:
+                break;
+            }
+
+            DestroyMenu(hMenu);
+        }
+    }
+    else if (uMsg == WM_REBUILDTOOLBAR)
+    {
+        Shell_NotifyIcon(NIM_ADD, &nid);
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+BOOL GetHostInformation()
+{
+    BOOL bSuccess = FALSE;
+    int argc = 0;
+    LPWSTR *argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+    if (argc < 3)
+    {
+        MessageBox(NULL, TEXT("获取主机信息失败，请加参数运行\r\n\r\n第一个参数为主机地址或ip\r\n第二个参数为端口"), NULL, MB_SYSTEMMODAL | MB_ICONERROR);
+    }
+    else
+    {
+        if (!IsPortW(argv[2], &g_usPort))
+        {
+            MessageBoxFormat(NULL, NULL, MB_SYSTEMMODAL | MB_ICONERROR, TEXT("“%ls”不是合法的端口号"), argv[2]);
+        }
+        else
+        {
+            char szDomainNameOrIpAddress[1024];
+
+            wnsprintfA(szDomainNameOrIpAddress, sizeof(szDomainNameOrIpAddress), "%S", argv[1]);
+
+            if (IsIpAddress(szDomainNameOrIpAddress))
+            {
+                g_dwAddress = inet_addr(szDomainNameOrIpAddress);
+                bSuccess = TRUE;
+            }
+            else
+            {
+                struct hostent *pHostEnt = gethostbyname(szDomainNameOrIpAddress);
+
+                if (pHostEnt == NULL)
+                {
+                    MessageBoxFormat(NULL, NULL, MB_SYSTEMMODAL | MB_ICONERROR, TEXT("“%ls”无法正确解析"), argv[1]);
+                }
+                else
+                {
+                    g_dwAddress = *(DWORD *)pHostEnt->h_addr_list[0];
+                    lstrcpynA(g_szDomainName, szDomainNameOrIpAddress, sizeof(g_szDomainName));
+                    bSuccess = TRUE;
+                }
+            }
+        }
+    }
+
+    if (argv != NULL)
+    {
+        LocalFree(argv);
+    }
+
+    return bSuccess;
+}
+
+DWORD __stdcall SnifferProc(LPVOID lpParam)
+{
+    BOOL bActive = FALSE;
+    HWND hMainWindow = (HWND)lpParam;
+    sockaddr_in sin;
+
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = g_dwAddress;
+    sin.sin_port = htons(g_usPort);
+
+    if (!IsWindow(hMainWindow))
+    {
+        return 0;
+    }
+ 
+    while (TRUE)
+    {
+        SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        while (sock == INVALID_SOCKET && IsWindow(hMainWindow))
+        {
+            Sleep(3000);
+
+            sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        }
+
+        if (!IsWindow(hMainWindow))
+        {
+            closesocket(sock);
+            break;
+        }
+
+        int ret = connect(sock, (const sockaddr *)&sin, sizeof(sin));
+
+        if (!IsWindow(hMainWindow))
+        {
+            closesocket(sock);
+            break;
+        }
+
+        if (ret != 0)
+        {
+            if (bActive)
+            {
+                bActive = FALSE;
+                PostMessage(hMainWindow, WM_REPORT, bActive, TRUE);
+            }
+
+            Sleep(3000);
+        }
+        else
+        {
+            if (!bActive)
+            {
+                bActive = TRUE;
+                PostMessage(hMainWindow, WM_REPORT, bActive, TRUE);
+            }
+
+            SetSockKeepAlive(sock, 1, 3000, 5000);
+
+            while (IsWindow(hMainWindow))
+            {
+                char szBuffer[4096];
+
+                ret = recv(sock, szBuffer, sizeof(szBuffer), 0);
+
+                if (ret <= 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        closesocket(sock);
+    }
+
+    return 0;
+}
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd)
+{
+    if (!InitWinSock(MAKEWORD(2, 2)))
+    {
+        MessageBox(NULL, TEXT("初始化网络失败"), NULL, MB_SYSTEMMODAL | MB_ICONERROR);
+        return 0;
+    }
+
+    if (!GetHostInformation())
+    {
+        return 0;
+    }
+
+    WNDCLASSEX wcex = {sizeof(wcex)};
+    wcex.lpfnWndProc = NotifyWndProc;
+    wcex.lpszClassName = NOTIFYWNDCLASS;
+    wcex.hInstance = hInstance;
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+
+    RegisterClassEx(&wcex);
+
+    HWND hMainWnd = CreateWindowEx(
+        0,
+        NOTIFYWNDCLASS,
+        NOTIFYWNDCLASS,
+        WS_OVERLAPPEDWINDOW,
+        0,
+        0,
+        433,
+        334,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+        );
+
+//     ShowWindow(hMainWnd, SW_SHOW);
+//     UpdateWindow(hMainWnd);
+
+    HANDLE hThread = CreateThread(NULL, 0, SnifferProc, (LPVOID)hMainWnd, 0, NULL);
+
+    MSG msg;
+
+    while (TRUE)
+    {
+        int nRet = GetMessage(&msg, NULL, 0, 0);
+
+        if (nRet <= 0)
+        {
+            break;
+        }
+
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    //工作线程不管了
+
+    return 0;
+}
+
+#if _MSC_VER <= 1200
+#ifndef _DEBUG
+
+#pragma comment(linker, "/Entry:s")
+#pragma comment(linker, "/Opt:nowin98")
+
+void s()
+{
+    ExitProcess(_tWinMain(GetModuleHandle(NULL), NULL, NULL, SW_SHOW));
+}
+
+#endif
+#endif
